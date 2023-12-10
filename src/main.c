5854dcd1b0e4b38b3dff317dcb023716c29f7965
Fabricio Nascimento <f.f.nascimento9@gmail.com>
2023-12-09 23:40:06 -0500

    default gitignore for C projects

--- src/main.c
+++ src/main.c
@@ -1,71 +1,84 @@
-#include <SDL2/SDL.h>
-#include <SDL2/SDL_render.h>
-#include <stdbool.h>
 #include <stdio.h>
+#include <SDL2/SDL.h>
+#include <stdbool.h>
 
-SDL_Window *window = NULL;
-SDL_Renderer *renderer = NULL;
+SDL_Window* window = NULL;
+SDL_Renderer* renderer = NULL;
 bool is_running = false;
 
-void setup() {}
+bool initialize_window(void) {
+    if (SDL_Init(SDL_INIT_EVERYTHING) != 0) {
+        printf("Error initializing SDL.\n");
+        return false;
+    }
+
+    window = SDL_CreateWindow(
+        "Janela",
+        SDL_WINDOWPOS_CENTERED,
+        SDL_WINDOWPOS_CENTERED,
+        1024,
+        720,
+        SDL_WINDOW_BORDERLESS
+    );
+
+    if (!window) {
+        printf("Failed to initialize window\n");
+        return false;
+    }
+
+    renderer = SDL_CreateRenderer(window, -1, 0);
+
+    if (!renderer) {
+        printf("Failed to initialize renderer\n");
+        return false;
+    }
+
+    return true;
+}
+
+void setup(void) {}
 
 void process_input() {
-  SDL_Event event;
-  SDL_PollEvent(&event);
+    SDL_Event event;
+    SDL_PollEvent(&event);
 
-  switch (event.type) {
-  case SDL_QUIT:
-    is_running = false;
-  case SDL_KEYDOWN:
-    if (event.key.keysym.sym == SDLK_ESCAPE) {
-      is_running = false;
+    switch (event.type) {
+        // when the window is closed this event gets called
+        case SDL_QUIT:
+            is_running=false;
+            break;
+
+        case SDL_KEYDOWN:
+            if (event.key.keysym.sym == SDLK_ESCAPE) {
+                is_running = false;
+            }
+            break;
     }
-  }
 }
-
-void render() {
-  SDL_SetRenderDrawColor(renderer, 255, 0, 0, 255);
-  SDL_RenderClear(renderer);
-  SDL_RenderPresent(renderer);
-}
-
 void update() {}
 
-bool initialize_window(void) {
-  if (SDL_Init(SDL_INIT_EVERYTHING) != 0) {
-    fprintf(stderr, "Error initializing SDL.\n");
-    return false;
-  }
+void render() {
+    SDL_SetRenderDrawColor(renderer, 255, 150, 150, SDL_ALPHA_OPAQUE);
+    SDL_RenderClear(renderer);
+    // colorbuffer - an array of colors
+    // 0x = hex number
+    // 0xFFFF0000
+    //   A R G B
+    // ...
 
-  window =
-      SDL_CreateWindow(NULL, SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED,
-                       600, 600, SDL_WINDOW_BORDERLESS);
-
-  if (!window) {
-    fprintf(stderr, "Error creating SDL window");
-    return false;
-  }
-
-  renderer = SDL_CreateRenderer(window, -1, 0);
-
-  if (!renderer) {
-    fprintf(stderr, "Error creating SDL renderer.\n");
-    return false;
-  }
-
-  return true;
-};
+    SDL_RenderPresent(renderer);
+}
 
 int main(int argc, char *argv[]) {
-  // TODO: Create SDL Window
-  is_running = initialize_window();
+    is_running = initialize_window();
 
-  setup();
+    setup();
 
-  while (is_running) {
-    process_input();
-    update();
-    render();
-  }
-  return 0;
+    while (is_running) {
+        process_input();
+        update();
+        render();
+    }
+
+    return 0;
 }
