e002263de2632d8b887c0cef9ca61596ec3365c1
Fabricio Nascimento <f.f.nascimento9@gmail.com>
2023-12-12 00:27:30 -0500

    adding color_buffer and fullscreen mode

--- src/main.c
+++ src/main.c
@@ -1,10 +1,17 @@
 #include <stdio.h>
-#include <SDL2/SDL.h>
 #include <stdbool.h>
+#include <stdint.h>
+#include <SDL2/SDL.h>
+
+int window_width = 1280;
+int window_height = 720;
 
 SDL_Window* window = NULL;
 SDL_Renderer* renderer = NULL;
+SDL_Texture* color_buffer_texture = NULL;
 bool is_running = false;
+
+uint32_t* color_buffer = NULL;
 
 bool initialize_window(void) {
     if (SDL_Init(SDL_INIT_EVERYTHING) != 0) {
@@ -12,12 +19,23 @@ bool initialize_window(void) {
         return false;
     }
 
+    // use SDL to query what is the fullscreen max. width and height
+    SDL_DisplayMode display_mode;
+    SDL_GetCurrentDisplayMode(0, &display_mode);
+
+    if (!display_mode.w) {
+        printf("Display mode couldn't be retrieved\n");
+    }
+
+    window_width = display_mode.w;
+    window_height = display_mode.h;
+
     window = SDL_CreateWindow(
         "Janela",
         SDL_WINDOWPOS_CENTERED,
         SDL_WINDOWPOS_CENTERED,
-        1024,
-        720,
+        window_width,
+        window_height,
         SDL_WINDOW_BORDERLESS
     );
 
@@ -33,10 +51,34 @@ bool initialize_window(void) {
         return false;
     }
 
+    // this will actually use fullscreen
+    // this allows us to use a lower resolution for the window while maintaining it as fullscreen mode
+    SDL_SetWindowFullscreen(window, SDL_WINDOW_FULLSCREEN);
+
     return true;
 }
 
-void setup(void) {}
+void setup(void) {
+    color_buffer = (uint32_t*) malloc(
+        sizeof(uint32_t) * window_width * window_height
+    );
+
+    if (!color_buffer) {
+        printf("Color buffer couldn't be initialized");
+    }
+
+    color_buffer_texture = SDL_CreateTexture(
+        renderer,
+        SDL_PIXELFORMAT_ARGB8888,
+        SDL_TEXTUREACCESS_STREAMING,
+        window_width,
+        window_height
+    );
+
+    if (!color_buffer_texture) {
+        printf("Color buffer texture couldn't be initialized\n");
+    }
+}
 
 void process_input() {
     SDL_Event event;
@@ -45,7 +87,7 @@ void process_input() {
     switch (event.type) {
         // when the window is closed this event gets called
         case SDL_QUIT:
-            is_running=false;
+            is_running = false;
             break;
 
         case SDL_KEYDOWN:
@@ -57,6 +99,18 @@ void process_input() {
 }
 void update() {}
 
+void clear_color_buffer(uint32_t color) {
+    for(int i = 0; i < window_height * window_width; i++) {
+        color_buffer[i] = color;
+    }
+}
+
+
+void render_color_buffer() {
+    SDL_UpdateTexture(color_buffer_texture, NULL, color_buffer, window_width * sizeof(uint32_t));
+    SDL_RenderCopy(renderer, color_buffer_texture, NULL, NULL);
+}
+
 void render() {
     SDL_SetRenderDrawColor(renderer, 255, 150, 150, SDL_ALPHA_OPAQUE);
     SDL_RenderClear(renderer);
@@ -65,8 +119,18 @@ void render() {
     // 0xFFFF0000
     //   A R G B
     // ...
+    clear_color_buffer(0xFFFFFF00);
+    render_color_buffer();
 
     SDL_RenderPresent(renderer);
+}
+
+
+void destroy_window() {
+    free(color_buffer);
+    SDL_DestroyRenderer(renderer);
+    SDL_DestroyWindow(window);
+    SDL_Quit();
 }
 
 int main(int argc, char *argv[]) {
@@ -79,6 +143,8 @@ int main(int argc, char *argv[]) {
         update();
         render();
     }
+
+    destroy_window();
 
     return 0;
 }
