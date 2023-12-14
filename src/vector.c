03bbcc2ec73da967797694abca5add8d138c7424
Fabricio Nascimento <f.f.nascimento9@gmail.com>
2023-12-14 12:36:56 -0500

    feat: adding vec2 and vec3 structs

---
+++ src/vector.c
@@ -0,0 +1,7 @@
+//
+// Created by ffnas on 12/14/2023.
+//
+
+#include "vector.h"
+
+// TODO: implementation for our vector functionalities here
---
+++ src/vector.h
@@ -0,0 +1,21 @@
+//
+// Created by ffnas on 12/14/2023.
+//
+
+#ifndef VECTOR_H
+#define VECTOR_H
+
+typedef struct {
+    float x;
+    float y;
+} vec2_t;
+
+typedef struct {
+    float x;
+    float y;
+    float z;
+} vec3_t;
+
+// TODO: add functionality to manipulate vectors
+
+#endif //VECTOR_H
